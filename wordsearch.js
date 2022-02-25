0774335aef4acde96b643f38eb3b77e6a8f5be64
Kanem <kanemdeng@email.com>
2022-02-25 12:30:46 -0800

    added wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
 
-module.exports = wordSearch
+  const transposeLetters = transpose(letters);
+  const horizontalJoin1 = transposeLetters.map(ls => ls.join(''));
+  for (const l of horizontalJoin1) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+    
+
+const transpose = function(matrix) {
+  let resultArr = [];
+  
+  for (let i = 0; i < matrix[0].length; i++) {
+    let rows = [];
+    for (let j = 0; j < matrix.length; j++) {
+      rows.push(matrix[j][i]);
+    }
+    resultArr.push(rows);
+  }
+  return resultArr;
+};
+
+module.exports = wordSearch;
